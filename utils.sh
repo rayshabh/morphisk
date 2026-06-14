57e08eea1e066643f6b51f67c374fcb4e6a10fc3
rayshabh <khargareeshav@gmail.com>
2026-06-14 20:46:35 +0530

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -819,7 +819,7 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=j-hc
+author=rayshabh
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MODULE_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
